f02371d712298e5f62664e8c99af9bd52df2aa1a
tillkuhn <till.kuhn@dbschenker.com>
2015-06-24 09:31:24 +0200

    use minified js libs, web port to 9001

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var gzippo = require('gzippo');
 var express = require('express');
 var morgan = require('morgan');
 var app = express();
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 9001;
 
 app.use(morgan('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
